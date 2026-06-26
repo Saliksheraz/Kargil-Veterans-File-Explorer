a25801279fe0d193dd49c471328378339283ae47
Salik Sheraz <saliksheraz97@gmail.com>
2026-06-26 17:13:11 +0500

    Updates

--- adm/views.py
+++ adm/views.py
@@ -14,7 +14,19 @@ def folder_view(request, pk):
     folder = Folders.objects.get(id=pk)
     sub_folders = Folders.objects.filter(parent_folder=folder)
     files = Files.objects.filter(folder=folder)
-    return render(request, "adm/folder_view.html", {"sub_folders": sub_folders, "files":files, "folder": folder})
+    
+    breadcrumbs = []
+    curr = folder
+    while curr is not None:
+        breadcrumbs.insert(0, curr)
+        curr = curr.parent_folder
+        
+    return render(request, "adm/folder_view.html", {
+        "sub_folders": sub_folders, 
+        "files": files, 
+        "folder": folder,
+        "breadcrumbs": breadcrumbs
+    })
 
 @login_required
 def create_folder(request):
